07efe63230a45f0cbdade9a99b519be9dbbc2f1f
Sara VEY <vey.sara@optistech.com>
2024-07-06 11:12:29 +0700

    add

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,7 +3,7 @@ export default function AcmeLogo() {
   return (
     <div className='leading-none text-white flex flex-col items-center'>
       <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 0 24 24" width="150px" fill="pink"><rect fill="none" /><path d="M19,10c-1.1,0-2,0.9-2,2h-1V3L3,8v13h18v-9C21,10.9,20.1,10,19,10z M5,9.37l9-3.46V12H9v7H5V9.37z M19,19h-3v-3h-2v3h-3v-5 h8V19z" /></svg>
-      <h1 className="text-pink-600 text-2xl">Sarah's house</h1>
+      <h1 className="text-pink-600 text-2xl">Sarah&apos;s house</h1>
     </div>
   );
 }
